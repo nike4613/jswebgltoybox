2564ace8643cfd657200af40fc20ec7fc7890c3a
Anairkoen Schno <aaron.cirr.com@gmail.com>
2018-05-06 01:44:30 -0500

    Refactored `GLArrayBuffer`s to a more extensible format

--- app/js/gl/GLArrayBuffer.ts
+++ app/js/gl/GLArrayBuffer.ts
@@ -1,45 +1,96 @@
+import { TypedArray } from "../util/misc";
 import { WebGL, WebGLAttribLocation } from "./WebGL";
 
 export interface AttribPointerOptions {
     attribLocation: WebGLAttribLocation;
-    components: number; // num components per vert
     normalize: boolean;
 }
 
-export class GLFloatArrayBuffer {
+export type TypedArrayType = new (data: number[]) => TypedArray;
+export abstract class GLArrayBuffer {
     private _gl: WebGL;
     private _buf: WebGLBuffer;
+    private _glenumType: number;
+    private _arrayType: TypedArrayType;
+    private _usage: number;
 
-    constructor(gl: WebGL) {
+    protected constructor(gl: WebGL, enumtype: number, arraytype: TypedArrayType, usage: number) {
         this._gl = gl;
+
+        this._glenumType = enumtype;
+        this._arrayType = arraytype;
+        this._usage = usage;
+
+        this._buf = gl.gl.createBuffer();
+    }
+
+    setData(data: number[]): this {
+        const gl = this._gl.gl;
+
+        this.bindBuffer();
+        gl.bufferData(this._glenumType, new this._arrayType(data), this._usage);
+
+        return this;
+    }
+
+    bindBuffer() {
+        this._gl.gl.bindBuffer(this._glenumType, this._buf);
     }
     
-    init(): GLFloatArrayBuffer {
-        const gl = this._gl.gl;
-        this._buf = gl.createBuffer();
-
-        return this;
+    protected set _glType(type: number) {
+        this._glenumType = type;
     }
 
-    setData(data: number[]): GLFloatArrayBuffer {
-        const gl = this._gl.gl;
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this._buf);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
-
-        return this;
-    }
-
-    bindAttribPointer(opts: AttribPointerOptions) {
-        const gl = this._gl.gl;
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this._buf);
-        gl.vertexAttribPointer(opts.attribLocation, opts.components,
-                               gl.FLOAT, opts.normalize, 0, 0);
-        gl.enableVertexAttribArray(opts.attribLocation);
+    protected get _GL() {
+        return this._gl;
     }
 
     get buffer(): WebGLBuffer {
         return this._buf;
+    }
+}
+
+export class GLFloatArrayBuffer extends GLArrayBuffer {
+    private _components: number;
+
+    constructor(gl: WebGL) {
+        super(gl, gl.gl.ARRAY_BUFFER, Float32Array, gl.gl.STATIC_DRAW);
+    }
+
+    setComponents(num: number): GLFloatArrayBuffer {
+        this._components = num;
+        return this;
+    }
+
+    bindAttribPointer(opts: AttribPointerOptions) {
+        const gl = this._GL.gl;
+
+        this.bindBuffer();
+        gl.vertexAttribPointer(opts.attribLocation, this.components,
+                               gl.FLOAT, opts.normalize, 0, 0);
+        gl.enableVertexAttribArray(opts.attribLocation);
+    }
+
+    get components(): number {
+        return this._components;
+    }
+}
+
+export abstract class GLElementArrayBuffer extends GLArrayBuffer {
+    private _type: number;
+
+    constructor(gl: WebGL, type: TypedArrayType, gltype: number) {
+        super(gl, gl.gl.ELEMENT_ARRAY_BUFFER, type, gl.gl.STATIC_DRAW);
+        this._type = gltype;
+    }
+
+    get type(): number {
+        return this._type;
+    }
+}
+
+export class GLUint16ElementArrayBuffer extends GLElementArrayBuffer {
+    constructor(gl: WebGL) {
+        super(gl, Uint16Array, gl.gl.UNSIGNED_SHORT);
     }
 }
