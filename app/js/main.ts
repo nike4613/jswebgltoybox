7476d1aed46d0c713ed80a12bd44f442d0ed6217
Anairkoen Schno <aaron.cirr.com@gmail.com>
2018-05-06 01:45:50 -0500

    Changed to drawing a wildly rotating cube intead of a square

--- app/js/main.ts
+++ app/js/main.ts
@@ -1,7 +1,7 @@
 import { mat4 } from "gl-matrix";
 import shader_frag from "../glsl/fragment.glsl";
 import shader_vert from "../glsl/vertex.glsl";
-import { GLFloatArrayBuffer } from "./gl/GLArrayBuffer";
+import { GLFloatArrayBuffer, GLUint16ElementArrayBuffer } from "./gl/GLArrayBuffer";
 import { Shader, ShaderProgram, ShaderType, WebGL } from "./gl/WebGL";
 import { addFrameHandler, startRender } from "./gl/rendering";
 import { DocumentLoad } from "./util/events";
@@ -27,15 +27,15 @@ async function main() {
 
     console.log("Loaded!");
 
-    let webgl = new WebGL();
+    const webgl = new WebGL();
     webgl.init(canvas);
 
-    let vert = Shader.create(ShaderType.Vertex).src(shader_vert).compileUsing(webgl);
-    let frag = Shader.create(ShaderType.Fragment).src(shader_frag).compileUsing(webgl);
+    const vert = Shader.create(ShaderType.Vertex).src(shader_vert).compileUsing(webgl);
+    const frag = Shader.create(ShaderType.Fragment).src(shader_frag).compileUsing(webgl);
     
-    let program = new ShaderProgram(frag, vert).linkWith(webgl);
+    const program = new ShaderProgram(frag, vert).linkWith(webgl);
 
-    let info = program.constructInfoStruct({
+    const info = program.constructInfoStruct({
         attribs: {
             vertexPosition: "aVertexPosition",
             vertexColor: "aVertexColor"
@@ -48,21 +48,68 @@ async function main() {
     });
     console.log(info);
 
-    let squarebuf = new GLFloatArrayBuffer(webgl)
-        .init()
+    const cubebuf = new GLFloatArrayBuffer(webgl)
         .setData([
-            1.0,  1.0,
-            -1.0,  1.0,
-            1.0, -1.0,
-            -1.0, -1.0,
-        ]);
-    let colorbuf = new GLFloatArrayBuffer(webgl)
-        .init()
+            // Front face
+            -1.0, -1.0,  1.0,
+            1.0, -1.0,  1.0,
+            1.0,  1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            // Back face
+            -1.0, -1.0, -1.0,
+            -1.0,  1.0, -1.0,
+            1.0,  1.0, -1.0,
+            1.0, -1.0, -1.0,
+            // Top face
+            -1.0,  1.0, -1.0,
+            -1.0,  1.0,  1.0,
+            1.0,  1.0,  1.0,
+            1.0,  1.0, -1.0,
+            // Bottom face
+            -1.0, -1.0, -1.0,
+            1.0, -1.0, -1.0,
+            1.0, -1.0,  1.0,
+            -1.0, -1.0,  1.0,
+            // Right face
+            1.0, -1.0, -1.0,
+            1.0,  1.0, -1.0,
+            1.0,  1.0,  1.0,
+            1.0, -1.0,  1.0,
+            // Left face
+            -1.0, -1.0, -1.0,
+            -1.0, -1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            -1.0,  1.0, -1.0,
+        ])
+        .setComponents(3);
+    const colorbuf = new GLFloatArrayBuffer(webgl)
+        .setComponents(4);
+    let colorarr: number[] = [];
+    const faceColors = [
+        [1.0,  1.0,  1.0,  1.0],    // Front face: white
+        [1.0,  0.0,  0.0,  1.0],    // Back face: red
+        [0.0,  1.0,  0.0,  1.0],    // Top face: green
+        [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
+        [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
+        [1.0,  0.0,  1.0,  1.0],    // Left face: purple
+    ];
+
+    for (var j = 0; j < faceColors.length; ++j) {
+        const c = faceColors[j];
+
+        // Repeat each color four times for the four vertices of the face
+        colorarr = colorarr.concat(c, c, c, c);
+    }
+    colorbuf.setData(colorarr);
+
+    const idxbuf = new GLUint16ElementArrayBuffer(webgl)
         .setData([
-            1.0,  1.0,  1.0,  1.0,    // white
-            1.0,  0.0,  0.0,  1.0,    // red
-            0.0,  1.0,  0.0,  1.0,    // green
-            0.0,  0.0,  1.0,  1.0,    // blue 
+            0,  1,  2,      0,  2,  3,    // front
+            4,  5,  6,      4,  6,  7,    // back
+            8,  9,  10,     8,  10, 11,   // top
+            12, 13, 14,     12, 14, 15,   // bottom
+            16, 17, 18,     16, 18, 19,   // right
+            20, 21, 22,     20, 22, 23,   // left
         ]);
 
     function draw(delta: number): void {
@@ -78,8 +125,10 @@ async function main() {
         GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
 
         const sine = (Math.sin(performance.now()/1000) + 1) / 2;
+        const sine2 = (Math.sin(performance.now()/1100) + 1) / 2;
+        const fovsine = (Math.sin(performance.now()/800) + 1) / 2
 
-        const fov = (((1-sine) * 45) + 45) * Math.PI / 180;   // in radians
+        const fov = ((fovsine * 45) + 45) * Math.PI / 180;   // in radians
         const aspect = GL.canvas.clientWidth / GL.canvas.clientHeight;
         const zNear = 0.1;
         const zFar = 100.0;
@@ -92,15 +141,21 @@ async function main() {
         mat4.translate(modelViewMatrix,     // destination matrix
             modelViewMatrix,     // matrix to translate
             [-0.0, 0.0, /* (sine + .1) */ -6.0]);  // amount to translate
+        mat4.rotate(modelViewMatrix,
+            modelViewMatrix,
+            sine*Math.PI*2,
+            [1,0,1]);
+        mat4.rotate(modelViewMatrix,
+            modelViewMatrix,
+            sine2*Math.PI*3,
+            [1,1,0]);
 
-        squarebuf.bindAttribPointer({
+        cubebuf.bindAttribPointer({
             attribLocation: info.attribLocations.vertexPosition, 
-            components: 2,
             normalize: false
         });
         colorbuf.bindAttribPointer({
             attribLocation: info.attribLocations.vertexColor,
-            components: 4,
             normalize: false
         });
 
@@ -116,9 +171,10 @@ async function main() {
             modelViewMatrix);
 
         {
+            idxbuf.bindBuffer(); // bind element buffer
+            const vertexCount = 36;
             const offset = 0;
-            const vertexCount = 4;
-            GL.drawArrays(GL.TRIANGLE_STRIP, offset, vertexCount);
+            GL.drawElements(GL.TRIANGLES, vertexCount, idxbuf.type, offset);
         }
     }
 
---
+++ app/js/util/misc.ts
@@ -0,0 +1,2 @@
+
+export type TypedArray = Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Float32Array | Float64Array | Uint8ClampedArray;
